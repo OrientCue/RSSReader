c861c88a7c005073ff6f7871daec55af20ffaee3
Arseniy Strakh <Arseniy_Strakh@epam.com>
2020-11-16 18:04:50 +0300

    config: remove UI tests, Main.storyboard and SceneDelegate. Set deployment target iOS 11.0. Enable MRC.

--- RSSReader/RSSReader/AppDelegate.m
+++ RSSReader/RSSReader/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -13,28 +14,27 @@
 
 @implementation AppDelegate
 
+#pragma mark - AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  // Override point for customization after application launch.
+  UIWindow *window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
+  self.window = window;
+  [window release];
+  ViewController *vc = [ViewController new];
+  vc.view.backgroundColor = UIColor.grayColor;
+  self.window.rootViewController = vc;
+  [vc release];
+  [self.window makeKeyAndVisible];
+
+
   return YES;
 }
 
+#pragma mark -
 
-#pragma mark - UISceneSession lifecycle
-
-
-- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
-  // Called when a new scene session is being created.
-  // Use this method to select a configuration to create the new scene with.
-  return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+- (void)dealloc {
+  [_window release];
+  [super dealloc];
 }
-
-
-- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
-  // Called when the user discards a scene session.
-  // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-  // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-}
-
 
 @end
